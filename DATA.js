cbc53030440545262115882d257ca57c4040dc98
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-24 21:07:47 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,32 @@
+export const item = {
+  "feed": [
+    {
+      "post_header": {
+        "user_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/123.png",
+        "user_name": "John Doe",
+        "menu": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/456.png"
+      },
+      "post_images": [
+        {
+          "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/49.png",
+          "like": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png",
+          "comment": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/202.png",
+          "share": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/303.png",
+          "save": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/404.png"
+        },
+        {
+          "post": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/567.png",
+          "like": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/68.png",
+          "comment": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/169.png",
+          "share": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png",
+          "save": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+        }
+      ],
+      "post_label": "My first post",
+      "post_description": "This is a dummy post",
+      "view_comments": "View all comments",
+      "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/456.png",
+      "comment1": "This is a comment"
+    }
+  ]
+}
